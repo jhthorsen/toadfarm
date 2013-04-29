7efadd1796926d38d583e8024f01b42d723edf3f
Jan Henning Thorsen <jhthorsen@cpan.org>
2013-04-30 01:59:53 +0200

    Add example setup for Ubuntu

--- lib/Toadfarm.pm
+++ lib/Toadfarm.pm
@@ -87,6 +87,14 @@ argument:
     # ...
   }
 
+=head1 EXAMPLE SETUP
+
+Look at L<https://github.com/jhthorsen/toadfarm/tree/etc/> for example
+resources which show how to start L<Toadfarm> on ubuntu. In addition, you can
+forward all traffic to the server using the "iptables" rule below:
+
+  $ iptables -A PREROUTING -i eth0 -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 8080
+
 =cut
 
 use Mojo::Base 'Mojolicious';
