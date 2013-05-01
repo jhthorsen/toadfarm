067c5936d5df92869b9a80a250a9d60c93148b37
Jan Henning Thorsen <jhthorsen@cpan.org>
2013-05-01 19:56:25 +0200

    0.04        * Add Reload plugin

--- lib/Toadfarm.pm
+++ lib/Toadfarm.pm
@@ -6,7 +6,7 @@ Toadfarm - One Mojolicious app to rule them all
 
 =head1 VERSION
 
-0.03
+0.04
 
 =head1 SYNOPSIS
 
@@ -106,7 +106,7 @@ L<Toadfarm::Plugin::Reload>.
 use Mojo::Base 'Mojolicious';
 use Mojo::Util 'class_to_path';
 
-our $VERSION = eval '0.03';
+our $VERSION = eval '0.04';
 
 =head1 METHODS
 
