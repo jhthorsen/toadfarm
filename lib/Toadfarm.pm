c975364731fae592277a0ef524b58b73c0265e53
Jan Henning Thorsen <jhthorsen@cpan.org>
2013-05-05 16:26:13 +0200

    Released version 0.07
           * Improved status page for reload plugin
           * Add support for custom error pages and assets

--- lib/Toadfarm.pm
+++ lib/Toadfarm.pm
@@ -6,7 +6,7 @@ Toadfarm - One Mojolicious app to rule them all
 
 =head1 VERSION
 
-0.06
+0.07
 
 =head1 SYNOPSIS
 
@@ -130,7 +130,7 @@ L<Toadfarm::Plugin::Reload>.
 use Mojo::Base 'Mojolicious';
 use Mojo::Util 'class_to_path';
 
-our $VERSION = eval '0.06';
+our $VERSION = eval '0.07';
 
 =head1 METHODS
 
