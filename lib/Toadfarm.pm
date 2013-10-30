b45ff06fe70ac28e9d2588754318bafde494befc
Jan Henning Thorsen <jhthorsen@cpan.org>
2013-10-30 19:01:54 +0100

    Released version 0.15
           * Allowing Ubic::Service::Toadfarm to start any hypnotoad
             application.

--- lib/Toadfarm.pm
+++ lib/Toadfarm.pm
@@ -6,7 +6,7 @@ Toadfarm - One Mojolicious app to rule them all
 
 =head1 VERSION
 
-0.014
+0.15
 
 =head1 SYNOPSIS
 
@@ -150,7 +150,7 @@ L<Toadfarm::Plugin::Reload>.
 use Mojo::Base 'Mojolicious';
 use Mojo::Util 'class_to_path';
 
-our $VERSION = '0.014';
+our $VERSION = '0.15';
 
 =head1 METHODS
 
