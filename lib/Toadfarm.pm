695640fbb92ffa27c954bca71d3ba34ffc5ab950
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-03-25 16:11:39 +0100

    Released version 0.42
           * Improved documentation
           * Add support for filtering on remote_address
           * Add support for filtering on remote_port
           * Add support for custom mount point

--- lib/Toadfarm.pm
+++ lib/Toadfarm.pm
@@ -6,7 +6,7 @@ Toadfarm - One Mojolicious app to rule them all
 
 =head1 VERSION
 
-0.41
+0.42
 
 =head1 DESCRIPTION
 
@@ -50,7 +50,7 @@ use Mojo::Base 'Mojolicious';
 use Mojo::Util 'class_to_path';
 use File::Which;
 
-our $VERSION = '0.41';
+our $VERSION = '0.42';
 
 $ENV{MOJO_CONFIG} = $ENV{TOADFARM_CONFIG} if $ENV{TOADFARM_CONFIG};
 
