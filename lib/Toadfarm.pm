1d1f5d31c55ee747a2ce98a17a9f7e71a6010606
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-01-21 11:44:36 +0100

    Released version 0.46
           * Add support for base url in AccessLog (X-Request-Base)
           * Object-Oriented Mojo::JSON API is DEPRECATED

--- lib/Toadfarm.pm
+++ lib/Toadfarm.pm
@@ -6,7 +6,7 @@ Toadfarm - One Mojolicious app to rule them all
 
 =head1 VERSION
 
-0.45
+0.46
 
 =head1 DESCRIPTION
 
@@ -50,7 +50,7 @@ use Mojo::Base 'Mojolicious';
 use Mojo::Util 'class_to_path';
 use File::Which;
 
-our $VERSION = '0.45';
+our $VERSION = '0.46';
 
 $ENV{MOJO_CONFIG} = $ENV{TOADFARM_CONFIG} if $ENV{TOADFARM_CONFIG};
 
