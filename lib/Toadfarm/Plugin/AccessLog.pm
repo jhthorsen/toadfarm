e031a71328df2b595687ba633d29ecf4dc724f9a
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-02-16 19:31:45 +0100

    Cannot conclude before finish event is seen

--- lib/Toadfarm/Plugin/AccessLog.pm
+++ lib/Toadfarm/Plugin/AccessLog.pm
@@ -32,38 +32,39 @@ sub register {
 
   $app->hook(
     before_dispatch => sub {
-      my $tx = $_[0]->tx;
+      my $tx     = $_[0]->tx;
+      my $reason = '';
 
       $tx->req->env->{t0} = [gettimeofday];
 
       if (my $stream = Mojo::IOLoop->stream($tx->connection)) {
         Scalar::Util::weaken($tx);
         $subscribers = $stream->subscribers('close');
-        unshift @$subscribers, sub { $tx and _log($tx, $log, 'close') };
+        unshift @$subscribers, sub { $reason ||= 'close' };
         $stream->unsubscribe('close');
         $stream->on(close => $_) for @$subscribers;
-        $stream->on(timeout => sub { $tx and _log($tx, $log, 'timeout') });
+        $stream->on(timeout => sub { $reason ||= 'timeout' });
       }
 
-      $tx->once(finish => sub { _log($tx, $log, 'finish') });
+      $tx->once(
+        finish => sub {
+          my $tx   = shift;
+          my $req  = $tx->req;
+          my $url  = $req->url->clone->to_abs;
+          my $code = $tx->res->code;
+
+          unshift @{$url->path->parts}, @{$url->base->path->parts};
+          $code ||= $reason eq 'close' ? '499' : $reason eq 'timeout' ? '504' : '000';
+          $url->userinfo(undef);
+          $log->info(
+            sprintf '%s %s %s %s %.4fs',
+            $req->env->{identity} || $tx->remote_address,
+            $req->method, $url, $code, tv_interval($req->env->{t0}),
+          );
+
+        }
+      );
     }
-  );
-}
-
-sub _log {
-  return if $_[0]->{_access_logged}++;
-  my ($tx, $log, $reason) = @_;
-  my $req  = $tx->req;
-  my $url  = $req->url->clone->to_abs;
-  my $code = $tx->res->code;
-
-  unshift @{$url->path->parts}, @{$url->base->path->parts};
-  $code ||= $reason eq 'close' ? '499' : $reason eq 'timeout' ? '504' : '000';
-  $url->userinfo(undef);
-  $log->info(
-    sprintf '%s %s %s %s %.4fs',
-    $req->env->{identity} || $tx->remote_address,
-    $req->method, $url, $code, tv_interval($req->env->{t0}),
   );
 }
 
