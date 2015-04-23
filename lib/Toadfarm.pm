e836a150eb76c888a4c9c24100c7459559084cbe
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-04-23 19:59:55 +0200

    Released version 0.49
     - Allow single application in DSL to be the only application

--- lib/Toadfarm.pm
+++ lib/Toadfarm.pm
@@ -6,7 +6,7 @@ Toadfarm - One Mojolicious app to rule them all
 
 =head1 VERSION
 
-0.48
+0.49
 
 =head1 DESCRIPTION
 
@@ -50,7 +50,7 @@ use Mojo::Base 'Mojolicious';
 use Mojo::Util 'class_to_path';
 use File::Which;
 
-our $VERSION = '0.48';
+our $VERSION = '0.49';
 
 $ENV{MOJO_CONFIG} = $ENV{TOADFARM_CONFIG} if $ENV{TOADFARM_CONFIG};
 
