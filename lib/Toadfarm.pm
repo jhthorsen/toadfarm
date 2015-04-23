27860b6e6c55884314104744ca5dedd49fcea36b
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-04-23 19:40:07 +0200

    Released version 0.48
     - Add support for defining applications using DSL
     - Remove Ubic::Service::Toadfarm

--- lib/Toadfarm.pm
+++ lib/Toadfarm.pm
@@ -6,7 +6,7 @@ Toadfarm - One Mojolicious app to rule them all
 
 =head1 VERSION
 
-0.47
+0.48
 
 =head1 DESCRIPTION
 
@@ -50,7 +50,7 @@ use Mojo::Base 'Mojolicious';
 use Mojo::Util 'class_to_path';
 use File::Which;
 
-our $VERSION = '0.47';
+our $VERSION = '0.48';
 
 $ENV{MOJO_CONFIG} = $ENV{TOADFARM_CONFIG} if $ENV{TOADFARM_CONFIG};
 
