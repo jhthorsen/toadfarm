e9c4d186370aded4833124b05fed01dab352036c
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-04-26 23:32:48 +0200

    Released version 0.54
     - Add run_as()
     - Unable to change user/group without setting TOADFARM_INSECURE=1
     - Unable to start as root without setting TOADFARM_INSECURE=1

--- lib/Toadfarm.pm
+++ lib/Toadfarm.pm
@@ -6,7 +6,7 @@ Toadfarm - One Mojolicious app to rule them all
 
 =head1 VERSION
 
-0.53
+0.54
 
 =head1 DESCRIPTION
 
@@ -153,7 +153,7 @@ use File::Spec;
 use File::Which;
 use constant DEBUG => $ENV{TOADFARM_DEBUG} ? 1 : 0;
 
-our $VERSION = '0.53';
+our $VERSION = '0.54';
 
 BEGIN {
   $ENV{TOADFARM_ACTION} //= $ENV{MOJO_APP_LOADER} ? 'load' : (@ARGV and $ARGV[0] =~ /^(reload|start|stop)$/) ? $1 : '';
