fc3ec946820e0f38e86ecd0849f6ca45c151cfcc
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-04-27 07:52:46 +0200

    Released version 0.55
     - Add support for Mojolicious::Plugin::SetUserGroup

--- lib/Toadfarm.pm
+++ lib/Toadfarm.pm
@@ -6,7 +6,7 @@ Toadfarm - One Mojolicious app to rule them all
 
 =head1 VERSION
 
-0.54
+0.55
 
 =head1 DESCRIPTION
 
@@ -158,7 +158,7 @@ use File::Spec;
 use File::Which;
 use constant DEBUG => $ENV{TOADFARM_DEBUG} ? 1 : 0;
 
-our $VERSION = '0.54';
+our $VERSION = '0.55';
 
 BEGIN {
   $ENV{TOADFARM_ACTION} //= $ENV{MOJO_APP_LOADER} ? 'load' : (@ARGV and $ARGV[0] =~ /^(reload|start|stop)$/) ? $1 : '';
